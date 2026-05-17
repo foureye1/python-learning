e12400451143edf0d791209083a0f95fd591f3d6
foureye1 <utdk256@gmail.com>
2026-05-17 15:45:58 +0530

    Added calculator program

---
+++ calculator.py
@@ -0,0 +1,13 @@
+num1 = int(input("enter a num1 : "))
+num2 = int(input("enter a num2 : "))
+
+print("addition=", num1 + num2 )
+print("substraction=", num1 -  num2 )
+print("multiplication=", num1 * num2 )
+print("remainder=", num1%num2)
+
+if num2 != 0:
+     print("division=", num1/num2 )
+
+else :
+     print("number can not be divided by o")
